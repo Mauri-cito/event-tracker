ddae4bde98ec85d20c95cff115eec3387560bdd4
Mauri <maurito11@gmail.com>
2021-05-13 12:42:18 -0300

    new implemented fileservice integrated into endpoint handlers

--- index.js
+++ index.js
@@ -1,75 +1,43 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const FileService = require('./services/fileService')
+const FileService = require("./services/fileService");
 const fileService = new FileService();
 const port = 3000;
 
-/* 
+/*
  * handling customer request using POST
  * in order to avoid GET request being
- * cached for any route intermediate 
+ * cached for any route intermediate
  */
 
 /* root endpoint returning full file content */
-app.post('/file/:filename', async (req, res) => {
+app.post("/file/:filename", async (req, res) => {
   try {
     const { filename } = req.params;
-    const result = await fileService.getContent(filename);
-    res.status(200).json({
-      status: 'ok',
-      data: result
-    });
-  } catch(err) {
+    const keyword = req.query.keyword;
+    await fileService.getContent(res, filename, -1, keyword);
+  } catch (err) {
     res.status(400).json({
-      status: 'failed',
-      err
+      status: "failed",
+      err,
     });
   }
-})
+});
 
 /* endpoint returning last n events for given file */
-app.post('/file/:filename/events/:n', async (req, res) => {
+app.post("/file/:filename/events/:n", async (req, res) => {
   try {
-    const { 
-      filename,
-      n
-    } = req.params;
-
-    const result = await fileService.getContent(filename, n);
-    res.status(200).json({
-        status: 'ok',
-        data: result
-    });
-  } catch(err) {
+    const { filename, n } = req.params;
+    const keyword = req.query.keyword;
+    await fileService.getContent(res, filename, n, keyword);
+  } catch (err) {
     res.status(400).json({
-      status: 'failed',
-      err
+      status: "failed",
+      err,
     });
   }
-})
-
-/* endpoint returning all matching keyword from last n events */
-app.post('/file/:filename/events/:n/keyword/:key', async (req, res) => {
-  try {
-    const { 
-      filename,
-      n,
-      key
-    } = req.params;
-
-    const result = await fileService.getContent(filename, n, key);
-    res.status(200).json({
-      status: 'ok',
-      data: result
-    });
-  } catch(err) {
-    res.status(400).json({
-      status: 'failed',
-      err
-    });
-  }
-})
+});
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}/`);
-})
+});
